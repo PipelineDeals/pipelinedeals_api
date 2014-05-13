3d49807358d8f988308d472b950dc326b0601a5f
Scott Gibson <sevgibson@gmail.com>
2014-05-13 13:53:36 -0400

    add optional ability to pass app_key for registered apps

--- lib/pipeline_deals/resource.rb
+++ lib/pipeline_deals/resource.rb
@@ -9,19 +9,25 @@ module PipelineDeals
       scope = arguments.slice!(0)
       options = arguments.slice!(0) || {}
 
-      if options[:params]
-        options[:params].merge!({api_key: PipelineDeals.api_key})
-      else
-        options.merge!({params: {api_key: PipelineDeals.api_key}})
-      end
+      self.add_params(options, app_key: PipelineDeals.app_key) if PipelineDeals.app_key
+      self.add_params(options, api_key: PipelineDeals.api_key)
 
       super(scope, options)
     end
 
     def save
+      prefix_options[:app_key] = PipelineDeals.app_key if PipelineDeals.app_key
       prefix_options[:api_key] = PipelineDeals.api_key
       self.include_root_in_json = true
       super
+    end
+
+    def self.add_params(options, params)
+      if options[:params]
+        options[:params].merge!(params)
+      else
+        options.merge!({params: params})
+      end
     end
   end
 end
