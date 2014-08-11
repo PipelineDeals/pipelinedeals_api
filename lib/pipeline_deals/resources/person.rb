906dc3d754453407570a87079ba9e3c5d6061ffe
Iuri Matias <iuri.matias@gmail.com>
2014-08-11 17:52:06 +0200

    remove unwritable attributes when saving person

--- lib/pipeline_deals/resources/person.rb
+++ lib/pipeline_deals/resources/person.rb
@@ -9,5 +9,12 @@ module PipelineDeals
     belongs_to :lead_source, class_name: PipelineDeals::LeadSource
     belongs_to :lead_status, class_name: PipelineDeals::LeadStatus
     has_many :predfined_contacts_tags, class_name: PipelineDeals::PredefinedContactsTag
+
+    READ_ONLY_ATTRIBUTES = %w{full_name company_name user lead_status lead_source image_mobile_url predefined_contacts_tags possible_notify_user_ids}
+
+    def save
+      attributes.delete_if { |k, _| READ_ONLY_ATTRIBUTES.include?(k) }
+      super
+    end
   end
 end
