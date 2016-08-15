a252bd48785748826cf4fb4bcd17e6de738bf81c
Dylan Egan <dylanegan@gmail.com>
2016-08-15 11:57:23 +0200

    Set include_root_in_json higher up. :bomb: :v:

--- lib/pipeline_deals/resource.rb
+++ lib/pipeline_deals/resource.rb
@@ -1,8 +1,17 @@
 module PipelineDeals
   class Resource < ActiveResource::Base
-    self.site = "https://api.pipelinedeals.com"
-    self.prefix = "/api/v3/"
     self.collection_parser = PipelineDeals::Collection
+    self.include_root_in_json = true
+    self.prefix = "/api/v3/"
+    self.site = "https://api.pipelinedeals.com"
+
+    def self.add_keys(hash)
+      hash[:api_key] = PipelineDeals.api_key unless PipelineDeals.account_key
+      hash[:account_key] = PipelineDeals.account_key if PipelineDeals.account_key
+
+      hash[:app_key] = PipelineDeals.app_key if PipelineDeals.app_key
+      hash[:app_version] = PipelineDeals.app_version if PipelineDeals.app_version
+    end
 
     def self.find(*arguments)
       scope = arguments.slice!(0)
@@ -15,16 +24,8 @@ module PipelineDeals
 
     def save
       PipelineDeals::Resource.add_keys(prefix_options)
-      self.include_root_in_json = true
+
       super
-    end
-
-    def self.add_keys(hash)
-      hash[:api_key] = PipelineDeals.api_key unless PipelineDeals.account_key
-      hash[:account_key] = PipelineDeals.account_key if PipelineDeals.account_key
-
-      hash[:app_key] = PipelineDeals.app_key if PipelineDeals.app_key
-      hash[:app_version] = PipelineDeals.app_version if PipelineDeals.app_version
     end
   end
 end
